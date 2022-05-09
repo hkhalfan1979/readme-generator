51b77faa350e78dd03810b3e3f7b45e8eea14ecf
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-05-09 19:13:06 -0400

    question and writing to file

---
+++ index.js
@@ -0,0 +1,75 @@
+
+const inquirer = require('inquirer'); // declare inquirier
+const fs = require('fs'); // declare file inpput
+const generateMarkdown = require('./generateMarkdown') // file location
+
+// Create array of question for readme file
+inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "Enter your project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter brief project description:"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How do you install your project(commands)?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What usage does this project have?"
+    },
+    {
+        type: "checkbox",
+        message: "Select your prject license?",
+        name: "license",
+        choices: [            
+            "Apache",
+            "MIT",
+            "LGPL",
+            "BSD3",
+            "None",
+        ],
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "What command should be run to install the dependencies?",
+        default: 'npm i',
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+        default: 'npm test',
+    },    
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your github username:"
+    },
+    {
+     type: "input",
+     name: "email",
+     message: "Enter your email:"
+ },
+])  // create a function to create readme
+     .then((data) => {
+          // TODO: Create a function to write README file
+          const filename = data.title.replace(' ', "").toLowerCase()
+          fs.writeFile(`${filename}.md`, generateMarkdown(data), (err) =>
+          err ? console.error(err) : console.log("Your readme has been generated!"))
+     })
+
+
+// Initialize
+function init() { }
+
+// call app
+init();
